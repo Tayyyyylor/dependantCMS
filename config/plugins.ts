dc9690c9ce13c055905c8f17a048fe180ba76005
Tayyyyylor <bryan.h.pro@gmail.com>
2024-12-29 14:17:06 +0100

    test to put back cloudfront

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,10 +3,9 @@ export default ({ env }: { env: (key: string, defaultValue?: string) => string }
     upload: {
         config: {
             provider: 'aws-s3',
-            rootPath: "",
             providerOptions: {
+                baseUrl: env('CDN_URL'),
                 s3Options: {
-                    baseUrl: env('CDN_URL'),
                     credentials: {
                         accessKeyId: env('AWS_KEY_ID'),
                         secretAccessKey: env('AWS_SECRET'),
