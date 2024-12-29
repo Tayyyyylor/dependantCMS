003cc2b5c607d7699ec3e594c28f13b8581de3af
Tayyyyylor <bryan.h.pro@gmail.com>
2024-12-29 12:55:46 +0100

    try some shit

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,10 +3,10 @@ export default ({ env }: { env: (key: string, defaultValue?: string) => string }
     upload: {
         config: {
             provider: 'aws-s3',
+            rootPath: "",
             providerOptions: {
                 s3Options: {
                     baseUrl: env('CDN_URL'),
-                    rootPath: "",
                     credentials: {
                         accessKeyId: env('AWS_KEY_ID'),
                         secretAccessKey: env('AWS_SECRET'),
